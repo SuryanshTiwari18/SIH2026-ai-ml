e0dc06d7f924b0adcee46904d718c534f89d6933
Suryansh Tiwari <suryanshtiwari1811@gmail.com>
2026-09-23 10:17:13 +0530

    some cleanup

--- scratch/check_h01_nbrs.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df_train = pd.read_parquet('data/tier4_results/train.parquet')
-sub = df_train[df_train['timestamp'] == '2026-07-10 20:00:00']
-print(sub[['station_id', 'temperature_c', 'pressure_hpa', 'humidity_pct', 'isolated_deviation']].head(10).to_string())
--- scratch/inspect_mahal_stats.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-stats = joblib.load('models/mahalanobis_stats.joblib')
-print("Keys in mahalanobis_stats:", stats.keys())
-print("Sample station_hour keys:", list(stats['station_hour'].keys())[:5])
-print("Sample zone_hour keys:", list(stats['zone_hour'].keys())[:5])
-print("Sample entry in station_hour:", stats['station_hour'][list(stats['station_hour'].keys())[0]])
-
